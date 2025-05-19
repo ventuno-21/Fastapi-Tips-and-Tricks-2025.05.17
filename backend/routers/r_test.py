ef1bb62883931bd9abbbbcbf40a7fdca37163640
ventuno-21 <yasna.pakdel@outlook.com>
2025-05-19 12:33:23 -0700

    Using json_schema_extra for changing Schema example of Swagger

--- backend/routers/r_test.py
+++ backend/routers/r_test.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Body
 from enum import Enum
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from routers.r_users import router as user_router
 from db.engine import Base, engine
 from contextlib import asynccontextmanager
@@ -22,13 +22,13 @@ class foodEnum(str, Enum):
 
 
 class Book:
-    id: int
-    title: str
-    author: str
-    description: str
-    rating: int
+    # id: int
+    # title: str
+    # author: str
+    # description: str
+    # rating: int
 
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id: int, title: str, author: str, description: str, rating: str):
         self.id = id
         self.title = title
         self.author = author
@@ -36,15 +36,82 @@ class Book:
         self.rating = rating
 
 
-BOOKS = [
+class BookRequest(BaseModel):
+    # id: Optional[int] = Field(title="id is not necessary to mention")
+    id: Optional[int] = None
+    title: str = Field(min_length=2)
+    author: str = Field(min_length=2)
+    description: str = Field(min_length=2, max_length=200)
+    rating: int = Field(gt=0, lt=6)
+
+    class Config:
+        """
+        You can pass a dict to json_schema_extra to add extra information to the JSON schema
+        """
+
+        json_schema_extra = {
+            "example": {
+                "title": "Vampire Dairy",
+                "author": "Who knows who I am",
+                "description": "A book about spooky girl inside a castle with magic wand",
+                "rating": 5,
+            }
+        }
+
+
+books = [
     Book(1, "t1", "a1", "d1", 4),
     Book(2, "t2", "a2", "d2", 4),
     Book(3, "t3", "a3", "d3", 1),
     Book(4, "t4", "a4", "d4", 2),
     Book(5, "t5", "a5", "d5", 5),
+    Book(6, "t6", "a6", "d6", 5),
 ]
 
 items_dict = [{"item_one": "one"}, {"item_two": "two"}, {"item_three": "three"}]
+
+
+@router.get("/books")
+async def all_books():
+    return books
+
+
+@router.post("/create-book")
+async def create_book(add_book=Body()):
+    """
+    This endpoint is based on Python class
+    &
+    It will not show SCHEMA in body of Swagger
+    """
+    books.append(add_book)
+    return add_book
+
+
+@router.post("/create-book2")
+async def create_book2(add_book: BookRequest):
+    """
+    This endpoint validation is based on Pydantic model
+    &
+    It will show Schema in body of Swagger
+    """
+    # print(f"=====> new book type: {type(add_book)}")
+    # We should add new_book to Book class, because out items in books list expected Book class type
+    # books.append(add_book)
+    new_book = Book(**add_book.model_dump())
+    books.append(assaign_correct_id(new_book))
+    print(f"=====> new book type after add it to Book class: {type(new_book)}")
+    print(new_book)
+    print(f"convert to dictonary: {new_book.__dict__}")
+
+    return new_book
+
+
+def assaign_correct_id(book: Book):
+    if len(books) > 0:
+        book.id = books[-1].id + 1
+    else:
+        book.id = 1
+    return book
 
 
 @router.get("/", description="Description of this function", deprecated=True)
