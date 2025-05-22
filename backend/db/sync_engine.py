5830db0d76cb69c9819e01def230b94e548c31b5
ventuno-21 <yasna.pakdel@outlook.com>
2025-05-22 07:51:47 -0700

    connect database to POSTGRESQL

--- backend/db/sync_engine.py
+++ backend/db/sync_engine.py
@@ -1,12 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgresql@127.0.0.1/Todo"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
